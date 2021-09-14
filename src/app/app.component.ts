f969fb650945555211ebfb2321a7349f3de8fff1
asadullahawan96 <asadullahawan96@gmail.com>
2021-09-14 13:04:22 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-java';
+  value1: number = 0;
+  value2: number = 0;
+  sum = 0;
+
+
+  getSum(): void {
+    this.sum = this.value1 + this.value2;
+  }
 }
